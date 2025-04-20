6e2f70dc3fa44bb25fcd1ec0f75aba3e0acb1523
gu <effyroth@gmail.com>
2025-04-20 14:59:41 +0800

    Update my_ak.py

--- my_ak.py
+++ my_ak.py
@@ -199,7 +199,9 @@ def stock_zh_a_spot_em() -> pd.DataFrame:
         "invt": "2",
         "fid": "f12",
         "fs": "m:0 t:6,m:0 t:80,m:1 t:2,m:1 t:23,m:0 t:81 s:2048",
-        "fields": f"{x}",
+        # "fields": f"{x}",
+        "fields": "f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,"
+        "f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152",
     }
     temp_df = fetch_paginated_data(url, params)
     # temp_df.columns = [
