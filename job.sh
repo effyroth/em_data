2d8a7ba0793f100b74e7435408b5a8379cc536d3
guzhen <guzhen@tantanapp.com>
2025-04-28 18:06:34 +0800

    更新代码

--- job.sh
+++ job.sh
@@ -1,3 +1,4 @@
+cd /Users/zhengu/trade/em_data
 git pull
 source .venv/bin/activate
 python -V
@@ -8,3 +9,5 @@ git add .
 git commit -m "更新代码"
 git push
 
+# 添加定时任务，每天17点执行job.sh
+(crontab -l 2>/dev/null; echo "0 17 * * * source $(pwd)/job.sh >> $(pwd)/em_data.log 2>&1 ") | crontab -
