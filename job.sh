509e8c66ae2fceb760cec6f70241bed4e1544331
guzhen <guzhen@tantanapp.com>
2025-04-30 17:00:14 +0800

    更新代码

--- job.sh
+++ job.sh
@@ -1,4 +1,5 @@
 cd /Users/zhengu/trade/em_data
+proxyon
 git pull
 source .venv/bin/activate
 python -V
@@ -6,9 +7,10 @@ pip -V
 pip install -r requirements.txt
 proxyoff
 python my_ak.py
+proxyon
 git add .
 git commit -m "更新代码"
 git push
 
 # 添加定时任务，每天17点执行job.sh
-(crontab -l 2>/dev/null; echo "0 17 * * * source $(pwd)/job.sh >> $(pwd)/em_data.log 2>&1 ") | crontab -
+#(crontab -l 2>/dev/null; echo "0 17 * * * source $(pwd)/job.sh >> $(pwd)/em_data.log 2>&1 ") | crontab -
